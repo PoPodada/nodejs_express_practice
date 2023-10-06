1d2c18adb8faefe0d1234f636b821b54030c641c
PoPodada <goito.722@icloud.com>
2023-10-06 18:46:31 +0900

    sample.js

---
+++ sample.js
@@ -0,0 +1,11 @@
+// console.log("hello world")
+
+// var http = require("http");
+// var server = http.createServer(function(req,res){
+//     res.write("hello world\n");
+//     res.end();
+// }).listen(8080);
+
+// process.stdout.write("hello");
+
+var http =require("http");
