b02187a0d96c7ea087ac745d74e63028a61052ab
PoPodada <goito.722@icloud.com>
2023-10-06 22:18:31 +0900

    add nodejs express

--- sample.js
+++ sample.js
@@ -1,21 +1,3 @@
-// console.log("hello world")
-
-// var http = require("http");
-// var server = http.createServer(function(req,res){
-//     res.write("hello world\n");
-//     res.end();
-// }).listen(8080);
-
-// process.stdout.write("hello");
-
-// var http = require('http');
-// var server = http.createServer(function(req, res) {
-//   console.log("URL: " + req.url);
-//   console.log("Method: " + req.method);
-//   console.log("Header[Content-Type]: " + req.headers['content-type']);
-//   res.end();
-// }).listen(8080);
-
 var http = require('http');
 var url = require('url');
 
