f5e969b5cfca6a9f44741a4ce68e089a370a5e86
PoPodada <goito.722@icloud.com>
2023-10-06 22:10:12 +0900

    initial commit

--- sample.js
+++ sample.js
@@ -8,4 +8,19 @@
 
 // process.stdout.write("hello");
 
-var http =require("http");
+// var http = require('http');
+// var server = http.createServer(function(req, res) {
+//   console.log("URL: " + req.url);
+//   console.log("Method: " + req.method);
+//   console.log("Header[Content-Type]: " + req.headers['content-type']);
+//   res.end();
+// }).listen(8080);
+
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function(req, res) {
+  var url_parse = url.parse(req.url, true);
+  console.log(url_parse);
+  res.end();
+}).listen(8080);
